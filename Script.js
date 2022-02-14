935042feef37a823ce497ab6428c8cba680a3e3a
Hugo Gomes <79423600+Hug0mes@users.noreply.github.com>
2022-02-14 22:22:17 +0000

    14.02

--- Script.js
+++ Script.js
@@ -5,11 +5,11 @@ function register() {
     var name = document.getElementById("name").value;
     var email = document.getElementById("email").value;
     var password = document.getElementById("password").value;
-    var Rpassword = document.getElementById("Rpassword").value;
+    var rpassword = document.getElementById("rpassword").value;
     var username = document.getElementById("username").value; //formato ano-mes-dia
 
-    if (password == Rpassword) {
-        fetch('https://instagrama.pt/', {
+    if (password == rpassword) {
+        fetch('https://instagrama.pt/api/register', {
             method: 'post',
             headers: {
                 'Accept': 'application/json, text/plain, /',
@@ -31,7 +31,69 @@ function checkRegister(res) {
     console.log(res);
     if (res.message == "CREATED") {
         alert("Conta criada com sucesso!");
+
     } else {
         alert("Erro");
     }
 }
+
+function Login() {
+    alert("Register button pressed");
+  
+    var email = document.getElementById("email").value;
+    var password = document.getElementById("password").value;
+  
+    console.log(email);
+  
+    fetch('https://instagrama.pt/api/login', {
+        method: 'post',
+        headers: {
+            'Accept': 'application/json, text/plain, */*',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+  
+            email: email,
+            password: password
+         
+        })
+    }).then(res => res.json())
+        .then(res => checkLogin(res));
+  }
+  
+ /* function counter(x){
+    x = 1
+    h2.text(x)
+    x = x +1;
+    }
+  */
+    function checkLogin(res) {
+        console.log(res)
+        if (res.token != null) {
+            alert("Login com sucesso!");
+            localStorage.setItem("token", res.token); 
+            location.replace("index.html");
+        }
+        else {
+            alert("Erro!");
+        }
+    }
+
+
+  function getProfile(){
+    var token = localStorage.getItem("token");
+
+    if (token != null){
+        fetch('https://www.instagram.com/api/profile', {
+            method: 'get',
+            headers: {
+                'Accept': 'application/json, text/plain, */*',
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + token
+            }
+        }).then(res => res.json())
+            .then(res => console.log(res));
+    }
+
+}
+
